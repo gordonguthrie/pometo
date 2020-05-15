19456aa00104b554971e213e7cd141affa78b253
Developer <developer@12b6afd36b0a>
2020-05-15 10:45:03 +0000

    rejig of runtime and records - swithing to a reserved atom for rho records and also added support for asymettric invocation of dyadic functions and an output formatting command

--- src/parser_records.hrl
+++ src/parser_records.hrl
@@ -1,13 +1,14 @@
--record(rho, {
-              style      = eager,
-              indexed    = false,
-              type,
-              dimensions = [],
-              vals       = []
-             }).
+-record('__⍴__', {
+                  style      = eager, % [eager | lazy]
+                  indexed    = false,
+                  type,               % scalar_fn
+                  dimensions = [],
+                  vals       = []
+                 }).
 
 -record(expr, {
     type,
+    application,      % [monadic | dyadic]
     fn_name,
-    args     = []
+    args         = []
   }).
