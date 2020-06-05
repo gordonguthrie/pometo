460f40930756d7a99028abd9e71b88bcbe09d84e
Developer <developer@12b6afd36b0a>
2020-06-05 14:51:17 +0000

    start cutting tests over to new interface

--- src/pometo_test_helper.erl
+++ src/pometo_test_helper.erl
@@ -8,68 +8,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 run_interpreter_test(Code) when is_list(Code) ->
-    Lexed = [lex(X) || X <- Code],
-    ParseFn = fun(Lx, Bindings) ->
-        ?debugFmt("in ParseFn Lx is ~p~n- Bindings is ~p~n", [Lx, Bindings]),
-        case Lx of
-            {ok, []} ->
-                "";
-            {ok, Tokens} ->
-                Parsed = parse(Tokens),
-                ?debugFmt("helper: Parsed is ~p~n", [Parsed]),
-                {ok, NewBindings} = scope_dictionary:gets(),
-                ?debugFmt("helper: NewBindings is ~p~n", [NewBindings]),
-                CombinedBindings = pometo_runtime:combine_bindings(Bindings, NewBindings, Code),
-                ?debugFmt("helper: CombinedBindings is ~p~n", [CombinedBindings]),
-                _Subs = pometo_runtime:substitute_bindings(Parsed, CombinedBindings),
-                Results             = pometo_runtime:run_ast(Parsed),
-                ?debugFmt("helper: Results is ~p~n", [Results]),
-                FormattedResults    = pometo_runtime:format(Results),
-                FormattedResults;
-            {error, Errors} ->
-                {pometo_runtime:format_errors(Errors), #{}}
-        end
-    end,
-    lists:foldl(ParseFn, #{}, Lexed).
-
-lex(Code) ->
     try
-        ?debugFmt("in lex Code is ~p~n", [Code]),
-        _Lexed = pometo_lexer:get_tokens(Code)
+        R = pometo:interpret_TEST(Code),
+        ?debugFmt("R is ~p~n", [R]),
+        R
     catch Type:Errs ->
-        ?debugFmt("Test ~ts failed to lex~n- with ~p:~p", [Code, Type, Errs]),
+        ?debugFmt("Test ~ts failed to run in the interpreter~n- with ~p:~p", [Code, Type, Errs]),
         {errors, Errs}
     end.
 
-%run_interpreter2(Code) ->
-%    try
-%        ?debugFmt("Code is ~p~n", [Code]),
-%        Lexed = pometo_lexer:get_tokens(Code),
-%        ?debugFmt("Lexed is ~p~n", [Lexed]),
-%        {Parsed, Binding} = case Lexed of
-%            {ok, []} ->
-%                "";
-%            {ok, Tokens} ->
-%                ?debugFmt("Tokens is ~p~n", [Tokens]),
-%                {P, B} = parse(Tokens),
-%                ?debugFmt("Parsed is ~p~n", [P]),
-%                ?debugFmt("Bindings is ~p~n", [B]),
-%                {P, B};
-%                Subs = pometo_runtime:substitute_bindings(Parsed, Bindings),
-%                Results             = pometo_runtime:run_ast(Parsed),
-%                ?debugFmt("Results is ~p~n", [Results]),
-%                FormattedResults    = pometo_runtime:format(Results),
-%                FormattedResults;
-%            {error, Errors} ->
-%                {pometo_runtime:format_errors(Errors), #{}}
-%            end
-%    catch Type:Errs ->
-%        ?debugFmt("Test ~ts failed to run in the interpreter~n- with ~p:~p", [Code, Type, Errs]),
-%        {errors, Errs}
-%    end.
-
 run_compiler_test(Title, Code) when is_list(Code) ->
-    [run_compiler2(Title, X) || X <- Code].
+    "deefie".
+%    [run_compiler2(Title, X) || X <- Code].
 
 run_compiler2(Title, Code) when is_list(Code) ->
     try
@@ -89,7 +39,7 @@ run_compiler2(Title, Code) when is_list(Code) ->
             end
     catch Type:Errs ->
         ?debugFmt("Test ~ts failed to run in the compiler~n- with ~p:~p", [Code, Type, Errs]),
-        {error, Errs}
+        Errs
     end.
 
 parse(Tokenlist) ->
