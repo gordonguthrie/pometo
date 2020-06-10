b328bb2bbceba7875730a3c38afc829acb9cd245
Gordon Guthrie <gordon@hypernumbers.com>
2020-06-10 16:32:24 +0100

    noodling on the play runner

--- src/runner.erl
+++ src/runner.erl
@@ -11,20 +11,28 @@ run() ->
 	Codes = [
 %	         "Z🤣🤣😀😃😄😁😆😂😅🐜+K😑[&^😐¯P😍÷I😍×E😍😍😍😍 ← 1 2 3"
 %			 "1 2 + 3 4"
-	         "1 2 + 1 2 3"
+%	         "1 2 + 1 2 3"
 %			 "KORYTNAČKA ← 1 2 3"
 %			 "1 2 × ¯3 4",
 %			 "1 2 + 3 4",
 %			 "1 2 3 4 5 + 33",
 %			 "1 + 22 33 44 55",
 %			 "+ 0 1 2 ¯1"
-	         ],
+      "1 2 3 + 4 5 6",
+      "A ← 1 2 3\nA + 5 6 7",
+      "1 2 3 + 3 4",
+       "A ← 1 2 3\nB ← 5 6 7\nC ← A\nD ← B\nC ÷ D"
+      ],
 	[run(X) || X <- Codes],
 	ok.
 
-run(Code) -> Resp = pometo:compile_load_and_run_TEST(Code, "runner"),
-             io:format("compile_load_and_run_TEST returns ~ts~n", [Resp]),
-             ok.
+run(Code) ->
+  io:format("running ~ts~n", [Code]),
+  Resp1 = pometo:interpret_TEST(Code),
+  io:format("interpret_TEST returns ~ts~n", [Resp1]),
+  Resp2 = pometo:compile_load_and_run_TEST(Code, "runner"),
+  io:format("compile_load_and_run_TEST returns ~ts~n", [Resp2]),
+  ok.
 
 noodle() ->	make_module(bingo).
 
