4d0634e7bbb133c3c7f72b60965e36614ff0b2ac
Gordon Guthrie <gordon@hypernumbers.com>
2020-06-10 09:34:16 +0100

    fix up base16 dependencies in this little script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm src/pometo_lexer.erl
 rm src/pometo_parser.erl
-#rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/lfe/ebin -s runner run
-rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/lfe/ebin -s runner noodle
+rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/base16/ebin -s runner run
+#rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/base16/ebin -s runner noodle
