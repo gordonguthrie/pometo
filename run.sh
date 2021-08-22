b9bebddffd4fb1072ad6abb7c8c3a3846fb6baf9
Developer <developer@localhost>
2021-08-22 07:46:01 +0000

    delete the crash dump that the runner leaves

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@ rm src/pometo_lexer.erl
 rm src/pometo_parser.erl
 rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/base16/ebin -s runner run
 #rebar3 compile && erl -pa _build/default/lib/pometo/ebin/ -pa _build/default/lib/base16/ebin -s runner noodle
+
+# lets delete the erl_crash.dump that we generated becuz its shonky and embarrassing...
+rm erl_crash.dump
