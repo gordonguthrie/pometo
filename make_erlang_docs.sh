08fbc8b1540e03cca60c04a7f9df976e8ec2c3cf
Gordon Guthrie <gordon@hypernumbers.com>
2022-03-29 10:34:39 +0100

    added Erlang documentation to the docs site

--- make_erlang_docs.sh
+++ make_erlang_docs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rm doc/*
-erl -noshell -pa ebin  -run edoc_run application "pometo" "[{preprocess, true}]"
+rm docs/erlang_docs
+rebar3 compile
+erl -noshell -pa ebin  -run edoc_run application "pometo" "[{preprocess, true}, {application, pometo}, {dir, \"./docs/erlang_docs\"}, {todo, true}, {app_default, \"./erlang_docs\"}, {stylesheet, \"../assets/css/style.css\"}, {sort_functions, false}]"
+cp docs/images/pometo_logo_for_erlang_docs.png docs/erlang_docs/erlang.png
